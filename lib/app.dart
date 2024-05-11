e6fad999ad9434a6b12210d01401db71cd5b8546
Muhammad Rizqi <mrizqi070502@gmail.com>
2024-05-11 22:20:55 +0700

    feat: launcher logo, splash logo, color scheme

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,9 @@
+import 'package:arcibo/presentation/home/view/home_page.dart';
+import 'package:arcibo/util/config/theme.dart';
+import 'package:arcibo/util/l10n/l10n.dart';
+import 'package:arcibo/util/route/route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class App extends StatelessWidget {
   const App({
@@ -10,6 +15,20 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      useInheritedMediaQuery: true,
+      builder: (context, child) {
+        return MaterialApp(
+          locale: const Locale('in', 'ID'),
+          theme: lightTheme,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          navigatorObservers: [routeObserver],
+          onGenerateRoute: AppNavigator.onGenerateRoute,
+          initialRoute: HomePage.routeName,
+        );
+      },
+    );
   }
 }
---
+++ lib/util/config/theme.dart
@@ -0,0 +1,8 @@
+import 'package:arcibo/util/config/color_scheme.dart';
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: lightColorScheme,
+  fontFamily: 'Poppins',
+);
